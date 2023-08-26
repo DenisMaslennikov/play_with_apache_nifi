e2cf9f0495204175186f8a725d762621ac7c6e8b
Denis Maslennikov <ktotom7@yandex.ru>
2023-08-26 16:20:02 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Play with apache-nifi
+## Запуск
+1. Создание виртуального окружения:
+```commandline
+python -m venv venv
+```
+2. Активация виртуального окружения
+```commandline
+source venv\Scripts\activate 
+```
+3. Установка зависимостей
+```commandline
+pip install -r requirements.txt
+```
+4. Запуск докер контейнера с apache-nifi
+```commandline
+docker compose up
+```
+Apache-nifi доступен по адресу http://127.0.0.1:8080/nifi
+5. Запуск проекта
+```commandline
+streamlit run app.py
+```
+Проект доступен по ссылке http://127.0.0.1:8501
---
+++ app.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import nipyapi
+
+
+def deploy_template():
+    template = nipyapi.templates.get_template(st.session_state.template_name)
+    group = nipyapi.canvas.get_process_group(st.session_state.group_name)
+    nipyapi.templates.deploy_template(template_id=template.id, pg_id=group.id)
+
+
+nipyapi.config.nifi_config.host = 'http://127.0.0.1:8080/nifi-api'
+
+templates_name = [
+    template.template.name
+    for template in nipyapi.templates.list_all_templates().templates
+]
+
+groups_names = [
+    group.status.name
+    for group in nipyapi.canvas.list_all_process_groups()
+]
+
+st.title('Play with apache-nifi')
+
+with st.form('select nifi template'):
+    st.session_state['template'] = st.selectbox(
+        'Выбор шаблона',
+        options=templates_name,
+        key='template_name'
+    )
+    st.session_state['group'] = st.selectbox(
+        'Выбор группы',
+        options=groups_names,
+        key='group_name'
+    )
+    st.form_submit_button(
+        label='OK',
+        on_click=deploy_template,
+    )
